b7cfdaf1ad624fc191a3533f46209722cd933fc7
seah78 <githubseah.pro>
2022-02-01 15:17:09 +0100

    save

--- src/TrackingSystem/serializers.py
+++ src/TrackingSystem/serializers.py
@@ -6,3 +6,12 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        
+    def create(self, validated_data):
+        return User.objects.create_user(
+            validated_data["username"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+        )
